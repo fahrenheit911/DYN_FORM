0ed9dce9d4b4dbeeeb752a7cb39739bbd692b616
fahrenheit911 <golof@mail.ru>
2022-04-22 21:51:49 +0300

    new

--- TENNIS_DOM/main.js
+++ TENNIS_DOM/main.js
@@ -1,4 +1,5 @@
-let ballSpeed = 4;
+let ballSpeedX = 4;
+let ballSpeedY = 4;
 let fieldW = 500;
 let fieldH = 300;
 let ballW = 20;
@@ -25,7 +26,6 @@ function TTT() {
   //field.style.top = "0";
   let fieldCentreX = field.offsetLeft + field.offsetWidth / 2;
   let fieldCentreY = field.offsetTop + field.offsetHeight / 2;
-  console.log(fieldCentreY);
 
   let ball = document.createElement("div");
   field.appendChild(ball);
@@ -34,71 +34,84 @@ function TTT() {
   ball.style.height = ballH + "px";
   ball.style.backgroundColor = "red";
   ball.style.position = "absolute";
-  ball.style.zIndex = "1";
+  ball.style.zIndex = "999";
   let ballCentreX = ball.offsetLeft + ball.offsetWidth / 2;
   let ballCentreY = ball.offsetTop + ball.offsetHeight / 2;
   let ballPosX = fieldW / 2 - ballW / 2;
   let ballPosY = fieldH / 2 - ballH / 2;
   ball.style.top = ballPosY + "px";
   ball.style.left = ballPosX + "px";
-  //let ballSpeed = 1;
+  //let ballSpeedY = 1;
 
   let leftRaquette = document.createElement("div");
   field.appendChild(leftRaquette);
-  leftRaquette.style.width = fieldW * 0.014 + "px";
-  leftRaquette.style.height = fieldH * 0.33 + "px";
+  leftRaquette.style.width = fieldW * 0.015 + "px";
+  leftRaquette.style.height = fieldH * 0.3 + "px";
   leftRaquette.style.backgroundColor = "green";
   leftRaquette.style.position = "absolute";
-  leftRaquette.style.zIndex = "1";
+  leftRaquette.style.zIndex = "999";
   leftRaquette.style.border = "solid 1px black";
 
   let reightRaquette = document.createElement("div");
   field.appendChild(reightRaquette);
-  reightRaquette.style.width = fieldW * 0.014 + "px";
-  reightRaquette.style.height = fieldH * 0.33 + "px";
+  reightRaquette.style.width = fieldW * 0.015 + "px";
+  reightRaquette.style.height = fieldH * 0.3 + "px";
   reightRaquette.style.backgroundColor = "blue";
   reightRaquette.style.position = "absolute";
-  reightRaquette.style.zIndex = "1";
+  reightRaquette.style.zIndex = "0";
   reightRaquette.style.right = "0";
   reightRaquette.style.border = "solid 1px black";
-  //reightRaquette.style.float = "right";
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
   function start() {
     setInterval(tick, 40);
   }
 
   function tick() {
-    ballPosX += ballSpeed;
+    ballPosX += ballSpeedX / 5;
     ball.style.left = ballPosX + "px";
 
     //*гол справа
     if (ballPosX + ballW > fieldW) {
-      ballSpeed = ballSpeed;
+      ballSpeedX = 0;
+      ballSpeedY = 0;
       ballPosX = fieldW - ballW;
       ball.style.left = ballPosX + "px";
     }
     //*гол слева
     if (ballPosX < 0) {
-      ballSpeed = ballSpeed;
+      ballSpeedX = 0;
+      ballSpeedY = 0;
       ballPosX = 0;
       ball.style.left = ballPosX + "px";
     }
 
-    ballPosY += ballSpeed;
+    ballPosY += ballSpeedY;
     ball.style.top = ballPosY + "px";
 
     //* вылетел ли мяч ниже пола?
     if (ballPosY + ballH > fieldH) {
-      ballSpeed = -ballSpeed;
+      ballSpeedY = -ballSpeedY;
       ballPosY = fieldH - ballH;
       ball.style.top = ballPosY + "px";
     }
     //* вылетел ли мяч выше потолка?
     if (ballPosY < 0) {
-      ballSpeed = -ballSpeed;
+      ballSpeedY = -ballSpeedY;
       ballH.posY = 0;
       ball.style.top = ballPosY + "px";
     }
+  }
+
+  window.addEventListener("keypress", ggg, true);
+  function ggg(eo) {
+    eo = eo || window.event;
+    if (eo.key == "ArrowDown") alert("msg");
   }
 }
 
